5439a00dde3734eeae3a7fbd529be960f7bde6ea
Alexandr Noskov <san9nosko8@gmail.com>
2024-02-20 22:45:37 +0300

    refactor

--- main/models.py
+++ main/models.py
@@ -4,14 +4,14 @@ from django.conf import settings
 
 
 class Document(models.Model):
-    # Класс отправляемого документя
+    # Класс отправляемого документа
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='postman_user')  # Привязка к юзеру
 
     file_load = models.FileField(upload_to='documents/', verbose_name='Файл')  # Сам файл
-    check_file = models.BooleanField(default=False, verbose_name='Файл принят')  # Принятие файла, по у молчанию False
-    checkout_file = models.BooleanField(default=False, verbose_name='Файл Отклонен')  # Отклонение  файла, по у молчанию False
+    check_file = models.BooleanField(default=False, verbose_name='Файл принят')  # Принятие файла, по умолчанию False
+    checkout_file = models.BooleanField(default=False, verbose_name='Файл Отклонен')  # Отклонение  файла, по умолчанию False
 
-    # Строкое представления для разработчиков
+    # Строковое представления для разработчиков
     def __str__(self):
         return f'Документ {self.owner} Принятие - {self.check_file} Отклонение - {self.checkout_file}'
 
